0e20d1f1f7b9044f5d96205345517018e9e63480
Luke Lanza <82444449+aznalfl@users.noreply.github.com>
2023-07-07 10:46:18 +1000

    Update omnisend.php

--- form-actions/omnisend.php
+++ form-actions/omnisend.php
@@ -85,33 +85,30 @@ class Omnisend_Action_After_Submit extends \ElementorPro\Modules\Forms\Classes\A
 		$settings = $record->get( 'form_settings' );
 		$fields = $record->get( 'fields' );
 
-        // error_log(print_r($settings, true));
-        // error_log($fields);
-
-        // Prepare Omnisend data
-        $email = sanitize_email( $fields[ $settings['omnisend_email_field'] ]['value'] );
-        $firstName = isset( $settings['omnisend_first_name_field'] ) ? sanitize_text_field( $fields[ $settings['omnisend_first_name_field'] ]['value'] ) : '';
-        $lastName = isset( $settings['omnisend_last_name_field'] ) ? sanitize_text_field( $fields[ $settings['omnisend_last_name_field'] ]['value'] ) : '';
-        $sendWelcomeMessage = isset($settings['omnisend_send_welcome_message']) ? $settings['omnisend_send_welcome_message'] : false;
-
-        $omnisend_data = [
-            'identifiers' => [
-                [
-                    'type' => 'email',
-                    'channels' => [
-                        'email' => [
-                            'status' => 'subscribed'
-                        ]
-                    ],
-                    'id' => $email,
-                    'sendWelcomeMessage' => $sendWelcomeMessage === "yes" ? true : false // Ensuring sendWelcomeMessage is a boolean.
-                ]
-            ],
-            'tags' => isset( $settings['omnisend_tags'] ) ? array_map( 'trim', explode( ',', sanitize_text_field( $settings['omnisend_tags'] ) ) ) : [],
-            'firstName' => $firstName,
-            'lastName' => $lastName,
-        ];
-
+	        // Prepare Omnisend data
+	        $email = sanitize_email( $fields[ $settings['omnisend_email_field'] ]['value'] );
+	        $firstName = isset( $settings['omnisend_first_name_field'] ) ? sanitize_text_field( $fields[ $settings['omnisend_first_name_field'] ]['value'] ) : '';
+	        $lastName = isset( $settings['omnisend_last_name_field'] ) ? sanitize_text_field( $fields[ $settings['omnisend_last_name_field'] ]['value'] ) : '';
+	        $sendWelcomeMessage = isset($settings['omnisend_send_welcome_message']) ? $settings['omnisend_send_welcome_message'] : false;
+	
+	        $omnisend_data = [
+	            'identifiers' => [
+	                [
+	                    'type' => 'email',
+	                    'channels' => [
+	                        'email' => [
+	                            'status' => 'subscribed'
+	                        ]
+	                    ],
+	                    'id' => $email,
+	                    'sendWelcomeMessage' => $sendWelcomeMessage === "yes" ? true : false // Ensuring sendWelcomeMessage is a boolean.
+	                ]
+	            ],
+	            'tags' => isset( $settings['omnisend_tags'] ) ? array_map( 'trim', explode( ',', sanitize_text_field( $settings['omnisend_tags'] ) ) ) : [],
+	            'firstName' => $firstName,
+	            'lastName' => $lastName,
+	        ];
+	
 		// Send request to Omnisend
 		$response = wp_remote_post(
 			'https://api.omnisend.com/v3/contacts',
@@ -144,21 +141,21 @@ class Omnisend_Action_After_Submit extends \ElementorPro\Modules\Forms\Classes\A
 		}
 	}
 
-    public function on_export( $element ) {
-        unset(
-            $element['omnisend_api_key'],
-            $element['omnisend_email_field'],
-            $element['omnisend_first_name_field'],
-            $element['omnisend_last_name_field'],
-            $element['omnisend_tags'],
-            $element['omnisend_send_welcome_message']
-        );
-        return $element;
-    }
+	public function on_export( $element ) {
+		unset(
+			$element['omnisend_api_key'],
+			$element['omnisend_email_field'],
+			$element['omnisend_first_name_field'],
+			$element['omnisend_last_name_field'],
+			$element['omnisend_tags'],
+			$element['omnisend_send_welcome_message']
+		);
+		return $element;
+	}
 }
 
 function add_omnisend_action_after_plugins_loaded() {
-    \ElementorPro\Plugin::instance()->modules_manager->get_modules( 'forms' )->add_form_action( 'omnisend', new Omnisend_Action_After_Submit() );
+	\ElementorPro\Plugin::instance()->modules_manager->get_modules( 'forms' )->add_form_action( 'omnisend', new Omnisend_Action_After_Submit() );
 }
 
 add_action( 'plugins_loaded', 'add_omnisend_action_after_plugins_loaded' );
